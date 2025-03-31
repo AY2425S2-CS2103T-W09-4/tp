8cdfa126bb43ac2b7e855ba2c6b65898c545291d
soonami69 <timothysoonwf@gmail.com>
2025-03-31 23:14:15 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/wedding/DeleteWeddingCommand.java
+++ src/main/java/seedu/address/logic/commands/wedding/DeleteWeddingCommand.java
@@ -5,7 +5,6 @@ import static java.util.Objects.requireNonNull;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.Command;
 import seedu.address.logic.commands.CommandResult;
-import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.WeddingModel;
 import seedu.address.model.wedding.Wedding;
